837f926cbcef17715047744dc16b0a9dcd1683cd
ozikot <dendai0122@gmail.com>
2018-08-12 10:52:38 +0900

    test

---
+++ ABC105/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+
+
+    return 0;
+}
