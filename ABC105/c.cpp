17f9e8816db84aad58503ed547f974bc84d67c52
ozikot <dendai0122@gmail.com>
2018-08-12 16:50:57 +0900

    wakaranai...

---
+++ ABC105/c.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    string s = "";
+    
+
+
+    return 0;
+}
