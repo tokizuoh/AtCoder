2adb95726ef6c8e7e97087d7c0dd5076d8279cbb
ozikot <dendai0122@gmail.com>
2018-08-12 21:49:01 +0900

    submit

--- ABC105/c.cpp
+++ ABC105/c.cpp
@@ -6,8 +6,29 @@ int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
-    
+    int n;
+    cin >> n;
 
+    if(n == 0){
+        cout << 0 << endl;
+        return 0;
+    }
+
+    string s = "";
+    while(n != 1){
+        if(n % 2){
+            s += "1";
+            n--;
+        }else{
+            s += "0";
+        }
+        n /= -2;
+    }
+    s += "1";
+
+    reverse(s.begin(), s.end());
+    
+    cout << s << endl;
 
     return 0;
 }
