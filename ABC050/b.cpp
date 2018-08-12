a2776dfb169458291ff3af5ff86c28290ff067c4
ozikot <dendai0122@gmail.com>
2018-08-12 17:39:13 +0900

    B問題途中

---
+++ ABC050/b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    vector<int> T(n);
+    REP(i, n) cin >> T[i];
+
+    int m;
+    cin >> m;
+    vector<int> P(m), X(m);
+    REP(i, m) cin >> P[i] >> X[i];
+
+    
+
+    return 0;
+}
