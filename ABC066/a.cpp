93667af87bbf78a034d21771ce2512e74cced9df
ozikot <dendai0122@gmail.com>
2018-08-13 23:09:53 +0900

    submit

--- ABC066/a.cpp
+++ ABC066/a.cpp
@@ -6,7 +6,12 @@ int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
+    vector<int> A(3);
+    REP(i, 3) cin >> A[i];
 
+    sort(A.begin(), A.end());
+
+    cout << A[0] + A[1] << endl;
 
     return 0;
 }
