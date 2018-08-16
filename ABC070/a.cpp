7ec597e6e713e1292e0f238d2300334c8186a159
ozikot <dendai0122@gmail.com>
2018-08-16 13:16:25 +0900

    提出したA問題の公開と考察ファイルを追加

--- ABC070/a.cpp
+++ ABC070/a.cpp
@@ -6,7 +6,10 @@ int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
+    string n;
+    cin >> n;
 
+    cout << (n[0] == n[2] ? "Yes" : "No") << endl;
 
     return 0;
 }
---
+++ ABC070/cdr.md
@@ -0,0 +1,6 @@
+# [ABC070](https://beta.atcoder.jp/contests/abc070)  
+  
+## A問題  
+文字列型で処理するのが楽そうです  
+  
+## B問題
