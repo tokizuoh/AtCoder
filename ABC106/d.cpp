6b4c4989ce40eaed547f002db823dca40e92a6ef
ozikot <dendai0122@gmail.com>
2018-08-20 19:43:25 +0900

    二次元累積和で悩み厨

---
+++ ABC106/d.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n, m, q;
+    cin >> n >> m >> q;
+
+    int A[n + 2][n + 2] = {};
+    REP(i, m){
+        int l, r;
+        cin >> l >> r;
+        A[l][r + 1]++;
+    }
+
+    REP(i, n + 2){
+        REP(j, n + 2){
+            if(i) A[i][j] += A[i - 1][j];
+            if(j) A[i][j] += A[i][j - 1];
+        }
+    }
+
+    REP(i, n + 2){
+        REP(j, n + 2){
+            cout << A[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
