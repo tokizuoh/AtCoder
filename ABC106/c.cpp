0e58a3851f6189245a3edbf24c21e4ee4aba7b26
ozikot <dendai0122@gmail.com>
2018-08-20 17:44:46 +0900

    ACしたC問題を追加しました

--- ABC106/c.cpp
+++ ABC106/c.cpp
@@ -6,7 +6,18 @@ int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
+    string s;
+    cin >> s;
+    long long k;
+    cin >> k;
 
+    for(int i = 0; i < k; i++){
+        if(s[i] != '1'){
+            cout << s[i] << endl;
+            return 0;
+        }
+    }
+    cout << '1' << endl;
 
     return 0;
 }
