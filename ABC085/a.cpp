99d926b0289c19f28dcb62592f52d7bde20dbdb7
ozikot <dendai0122@gmail.com>
2018-08-23 11:00:21 +0900

    AC

---
+++ ABC085/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    string s;
+    cin >> s;
+    
+    s[3] = '8';
+    cout << s << endl;
+
+    return 0;
+}
