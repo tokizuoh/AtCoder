aa026f3fb1e689ccde1088e9e5b68c73b58bcc09
ozikot <dendai0122@gmail.com>
2018-08-26 15:29:40 +0900

    AC

--- ABC107/c.cpp
+++ ABC107/c.cpp
@@ -13,16 +13,7 @@ int main(){
 
     int ans = 1e9;
     for(int i = 0; i < n - k + 1; i++){
-        int cost = 0, tmp;
-        for(int j = i; j < i + k; j++){
-            if(j == i);
-            else cost += A[j] - tmp;
-            tmp = A[j];
-        }
-        if(0 < A[i] or A[i + k - 1] < 0){
-            cost += min(abs(A[i]), abs(A[i + k - i]));
-        }
-        cout << i << " " << cost<< endl;
+        int cost = A[i + k - 1] - A[i] + min(abs(A[i]), abs(A[i + k - 1]));
         ans = min(ans, cost);
     }
     cout << ans << endl;
