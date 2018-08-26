6718fd70c66a86992442558866689c0b8d58e195
ozikot <dendai0122@gmail.com>
2018-08-26 21:51:51 +0900

    AC

---
+++ ABC081/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    string s;
+    cin >> s;
+    cout << (int)count(s.begin(), s.end(), '1') << endl;
+
+    return 0;
+}
