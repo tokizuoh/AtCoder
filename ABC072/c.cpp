d126f7615e9d5add8f8a9b65e2818ca73e40f0a0
ozikot <dendai0122@gmail.com>
2018-09-08 20:52:07 +0900

    AC

---
+++ ABC072/c.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    unordered_map<int, int> mp;
+    REP(i, n){
+        int a;
+        cin >> a;
+        mp[a - 1]++;
+        mp[a]++;
+        mp[a + 1]++;
+    }
+
+    int maxi = -1e9;
+    for(auto x : mp){
+        maxi = max(maxi, x.second);
+    }
+
+    cout << maxi << endl;  
+
+    return 0;
+}
