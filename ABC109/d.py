1cb026c6e3a901e6179213e3f3a5e25213318529
ozikot <dendai0122@gmail.com>
2018-09-09 09:40:06 +0900

    Tako!さんの出力を真似させて頂きました

--- ABC109/d.py
+++ ABC109/d.py
@@ -7,29 +7,18 @@ def main():
         for j in range(W):
             if A[i][j] % 2:
                 if i < H - 1:
-                    B.append(i + 1)
-                    B.append(j + 1)
-                    B.append(i + 2)
-                    B.append(j + 1)
+                    B.append(map(str, [i + 1, j + 1, i + 2, j + 1]))
                     A[i][j] -= 1
                     A[i + 1][j] += 1
                     continue
                 elif j < W - 1:
-                    B.append(i + 1)
-                    B.append(j + 1)
-                    B.append(i + 1)
-                    B.append(j + 2)
+                    B.append(map(str, [i + 1, j + 1, i + 1, j + 2]))
                     A[i][j] -= 1
                     A[i][j + 1] += 1
     
-    print(int(len(B) / 4))
-    for i in range(0, len(B), 4):
-        for j in range(i, i + 4):
-            if j != i :
-                print("", B[j], end = "")
-            else:
-                print(B[j], end = "")
-        print()
+    print(len(B))
+    for b in B:
+        print(" ".join(b))
 
 if __name__ == "__main__":
     main()
