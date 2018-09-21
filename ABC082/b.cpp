1dc1780450c9a8b0e721780679e02a6396f08a52
ozikot <dendai0122@gmail.com>
2018-09-21 15:44:24 +0900

    AC

---
+++ ABC082/b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    string s, t;
+    cin >> s >> t;
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    reverse(t.begin(), t.end());
+
+    cout << (s < t ? "Yes": "No") << endl;
+
+    return 0;
+}
