65004786ca9a76740f38239b660dca791033d927
ozikot <dendai0122@gmail.com>
2018-10-01 17:45:52 +0900

    AC

---
+++ C_only/ABC/108.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    long long n, k;
+    cin >> n >> k;
+
+    vector<long long> cnt(k, 0);
+    for(int i = 1; i <= n; i++){
+        cnt[i % k]++;
+    }
+
+    long long ans = 0;
+    for(int i = 0; i < k; i++){
+        if(i * 2 % k != 0) continue;
+        ans += cnt[i] * cnt[i] * cnt[i];
+    }
+    cout << ans << endl;
+
+    return 0;
+}
