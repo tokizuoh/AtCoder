74d8e1f3ea5ef0cb38e7703b1c8f1d10f91d2755
ozikot <dendai0122@gmail.com>
2018-10-01 23:51:16 +0900

    AC

---
+++ C_only/ABC/105.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i = 0; i < n; i++)
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    string s = "";
+    while(n != 0){
+        if(n % 2 != 0){
+            n--;
+            s += "1";
+        }else{
+            s += "0";
+        }
+        n /= -2;
+    }
+    if(s == "") s = "0";
+
+    reverse(s.begin(), s.end());
+
+    cout << s << endl;
+
+    return 0;
+}
