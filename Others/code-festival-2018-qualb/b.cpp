61d68095990b23653c1313022c94179ffa1b13ef
ozikot <dendai0122@gmail.com>
2018-10-15 08:31:48 +0900

    AC

--- Others/code-festival-2018-qualb/b.cpp
+++ Others/code-festival-2018-qualb/b.cpp
@@ -7,45 +7,14 @@ int main(){
 
     int n, x;
     cin >> n >> x;
-
-    int maxi1 = -1e9;
-    map<int, pair<int, int>> mp;
+    int total = 0, max_b = 0;
     for(int i = 0; i < n; i++){
         int a, b;
         cin >> a >> b;
-        mp[a * b] = {a, b};
-        maxi1 = max(maxi1, a * b);
+        total += a * b;
+        max_b = max(max_b, b);
     }
-
-    vector<pair<int, int>> A;
-    for(auto y : mp){
-        if(y.first == maxi1){
-            A.push_back({y.second.first, y.second.second});
-        }
-    }
-
-    int maxi2 = -1e9, ta, tb;
-    for(auto y : A){
-        int tmp = (y.first + x) * y.second;
-        if(tmp > maxi2){
-            ta = y.first;
-            tb = y.second;
-            maxi2 = tmp;
-        }
-    }
-
-    int ans = 0;
-    bool f = true;
-    for(auto y : mp){
-        if(y.second.first == ta and y.second.second == tb and f){
-            ans += maxi2;
-            f = false;
-        }else{
-            ans += y.first;
-        }
-    }
-
-    cout << ans << endl;
+    cout << total + x * max_b << endl;
 
     return 0;
 }
