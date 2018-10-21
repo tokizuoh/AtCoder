4c7990399bc8987076fbcfc3e5e6bfd21586b72d
ozikot <dendai0122@gmail.com>
2018-10-21 15:04:36 +0900

    AC

---
+++ Others/qupc2018/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+
+    cout << 2010 + N * 4 << endl;
+
+    return 0;
+}
