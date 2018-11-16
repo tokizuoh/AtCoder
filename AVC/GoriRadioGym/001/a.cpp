4b726c1541306206340829afa42024315d3cdf01
ozikot <dendai0122@gmail.com>
2018-11-16 12:22:31 +0900

    AC

---
+++ AVC/GoriRadioGym/001/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    int ans = (1 + n) * n / 2;
+    cout << ans << endl;
+
+    return 0;
+}
