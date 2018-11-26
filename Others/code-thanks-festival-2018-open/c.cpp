46ca3e98d93d2771e9d5b56b794a0fa45a318310
ozikot <dendai0122@gmail.com>
2018-11-26 12:01:10 +0900

    未完成

---
+++ Others/code-thanks-festival-2018-open/c.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    vector<long long> X(n);
+    for(int i = 0; i < n; i++){
+        cin >> X[i];
+    }
+    sort(X.begin(), X.end());
+
+    long long ans = 0;
+    long long pre = 0;
+    for(int i = 1; i < n; i++){
+        long long len = (X[i] - X[i - 1]) * i;
+        ans += pre + len;
+        pre = 
+        // << "ans : " << ans << " | pre : " << pre << " | len : " << len << endl;
+    }
+
+    return 0;
+}
