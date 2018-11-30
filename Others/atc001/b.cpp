f9dd421dc12829a669ca4cbb67b001c7cb506434
ozikot <dendai0122@gmail.com>
2018-11-30 15:50:49 +0900

    AC unionfind

---
+++ Others/atc001/b.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct UnionFind{
+    vector<int> par;
+    UnionFind(int N) : par(N){
+        for(int i = 0; i < N; i++){
+            par[i] = -1;
+        }
+    }
+    int root(int x){
+        if(par[x] < 0) return x;
+        else return par[x] = root(par[x]);
+    }
+    void unite(int x, int y){
+        int rx = root(x);
+        int ry = root(y);
+        if(rx == ry) return;
+        else{
+            if(par[rx] > par[ry]) swap(rx, ry);
+            par[rx] += par[ry];
+            par[ry] = rx;
+       }
+    }
+    bool same(int x, int y){
+        return root(x) == root(y);
+    }
+    void check(){
+        for(auto x : par){
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n, q;
+    cin >> n >> q;
+
+    UnionFind UF(n);
+
+    for(int i = 0; i < q; i++){
+        int p, a, b;
+        cin >> p >> a >> b;
+        if(p == 0){
+            UF.unite(a, b);
+        }else{
+            if(UF.same(a, b)){
+                cout << "Yes" << endl;
+            }else{
+                cout << "No" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
