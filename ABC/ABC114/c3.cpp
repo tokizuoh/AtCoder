17710b2db3d7a839cb23eb560c2f0b0f7f4997bf
ozikot <dendai0122@gmail.com>
2018-12-04 00:20:39 +0900

    AC 再帰関数

--- ABC/ABC114/c3.cpp
+++ ABC/ABC114/c3.cpp
@@ -4,27 +4,53 @@ using namespace std;
 int n;
 vector<string> A;
 vector<char> B = {'3', '5', '7'};
+vector<int> C;
 
-void f(int rightRest, string s){
-    if(rightRest == 0){
+void f(int rightRestB, string s){
+    vector<bool> F(3, false);
+    for(int i = 0; i < s.length(); i++){
+        for(int j = 0; j < min(3, (int)s.size()); j++){
+            if(s[i] == B[j]){
+                F[j] = true;
+            }
+        }
+    }
+    if(count(F.begin(), F.end(), true) == 3){
+        A.push_back(s);
+    }
+    if(rightRestB == to_string(n).length()){
         return;
     }
     for(int i = 0; i < 3; i++){
-        f(rightRest - 1, B[i]);
+        f(rightRestB + 1, s + B[i]);
     }
-    A.push_back();
-    
+    return;
 }
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
-    int n;
     cin >> n;
 
-    string n_str = to_string(n);
-    f(n_str.length(), "");
+    f(0, "");
+
+    for(auto &x : A){
+        C.push_back(stoi(x));
+    }
+
+    sort(C.begin(), C.end());
+
+    int ans = 0;
+    for(auto &x : C){
+        if(x <= n){
+            ans++;
+        }else{
+            break;
+        }
+    }
+
+    cout << ans << endl;
 
     return 0;
 }
