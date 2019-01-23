acc71ffcf85c648e3d42924e19950bdddca771b1
ozikot <dendai0122@gmail.com>
2019-01-24 00:05:27 +0900

    AC

--- ABC/ABC114/b.py
+++ ABC/ABC114/b.py
@@ -1,6 +1,5 @@
-s = input()
-mini = int(1e9)
+s=input()
+mini=int(1e9)
 for i in range(len(s) - 2):
-    mini = min(mini, abs(int(s[i:i+3]) - 753))
-else:
-    print(mini)
+    mini = min(mini, abs(753-int(s[i:i+3])))
+print(mini)
