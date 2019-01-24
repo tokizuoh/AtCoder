945f13a5f7ea5099bf993e437fb084b7493ec00c
ozikot <dendai0122@gmail.com>
2019-01-25 00:08:26 +0900

    AC

--- ABC/ABC112/a.py
+++ ABC/ABC112/a.py
@@ -1,9 +1,5 @@
-N=int(input())
-if(N == 1):
-    print("Hello World")
-    exit()
+n = int(input())
+if n == 1:
+    print('Hello World')
 else:
-    a = int(input())
-    b = int(input())
-    print(a + b)
-    exit()
+    print(eval(input()+'+'+input()))
