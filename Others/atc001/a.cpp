ba29e608f24143fa7093de38885d2c7ffae892ef
ozikot <dendai0122@gmail.com>
2019-01-26 21:16:00 +0900

    AC

--- Others/atc001/a.cpp
+++ Others/atc001/a.cpp
@@ -2,48 +2,65 @@
 using namespace std;
 
 int h, w, sx, sy;
-const int MAX_h = 510, MAX_w = 510;
-char maze[MAX_h][MAX_w];
-bool reached[MAX_h][MAX_w];
-
+const int MAX = 510;
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
-bool DFS(int x, int y){
-    if(maze[x][y] == 'g') return true;
-    for(int i = 0; i < 4; i++){
-        int nx, ny;
-        nx = x + dx[i];
-        ny = y + dy[i];
-        if(nx < 0 or ny < 0 or nx >= h or ny >= w or maze[nx][ny] == '#' or reached[nx][ny]){
-            continue;
-        }
-        reached[nx][ny] = true;
-        if(DFS(nx, ny)) return true;
-    }
-    return false;
-}
+char C[MAX][MAX];
+
+bool seen[MAX][MAX];
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
     cin >> h >> w;
-
     for(int i = 0; i < h; i++){
         for(int j = 0; j < w; j++){
-            cin >> maze[i][j];
-            if(maze[i][j] == 's'){
+            cin >> C[i][j];
+            if(C[i][j] == 's'){
                 sx = i;
                 sy = j;
             }
         }
     }
 
-    cout << (DFS(sx, sy) ? "Yes" : "No") << endl;
 
+    stack<pair<int, int>> st;
+    st.push({sx, sy});
+    while(!st.empty()){
+        pair<int, int> t = st.top();
+        st.pop();
+        for(int i = 0; i < 4; i++){
+            int nx, ny;
+            nx = dx[i] + t.first;
+            ny = dy[i] + t.second;
+            if(nx < 0 or ny < 0 or nx >= h or ny >= w){
+                continue;
+            }
+            if(C[nx][ny] == '#' or seen[nx][ny]){
+                continue;
+            }
+            if(C[nx][ny] == 'g'){
+                cout << "Yes" << endl;
+                return 0;
+            }
+            seen[nx][ny] = true;
+            st.push({nx, ny});
 
+            /*
+            for(int i = 0; i < h; i++){
+                for(int j = 0; j < w; j++){
+                    cout << C[i][j] << ' ';
+                }
+                cout << endl;
+            }
+            cout << "----------------" << endl;
+            */
+        }
+    }
 
+    cout << "No" << endl;
 
     return 0;
 }
