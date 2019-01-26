cdf5461c6f98f69b528744c6f8f09bc3492d4006
ozikot <dendai0122@gmail.com>
2019-01-26 23:26:19 +0900

    AC なぜ

--- ABC/ABC074/d.cpp
+++ ABC/ABC074/d.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
 
 int main(){
     cin.tie(0);
@@ -10,20 +9,30 @@ int main(){
     cin >> n;
     long long A[n][n];
     bool F[n][n];
-    REP(i, n){
-        REP(j, n){
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
             cin >> A[i][j];
             F[i][j] = true;
         }
     }
 
-    REP(i, n){
-        REP(j, n){
-            REP(k, n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            for(int k = 0; k < n; k++){
+
+                // Aには最短距離が示されているはずなのでダメ
                 if(A[j][i] + A[i][k] < A[j][k]){
                     cout << -1 << endl;
                     return 0;
-                }else if(A[j][i] + A[i][k] == A[j][k] and i != j and i != k){
+                }
+
+                // i - k
+                // | /
+                // j
+                // このとき j-kのパスはとりのぞきj-i i-kを生き残らせる
+                //（多く生き残らせて存在する道路の長さの和を最小にするため）
+                if(A[j][k] == A[j][i] + A[i][k] and i != j and i != k){
                     F[j][k] = false;
                 }
             }
@@ -35,7 +44,6 @@ int main(){
         for(int j = i + 1; j < n; j++){
             if(F[i][j]) ans += A[i][j];
         }
-        cout << endl;
     }
 
     cout << ans << endl;
