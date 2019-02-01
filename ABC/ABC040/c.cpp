953a2e871b02c356201ff79ecbb71b8469c7d3ff
ozikot <dendai0122@gmail.com>
2019-02-01 09:42:00 +0900

    AC

--- ABC/ABC040/c.cpp
+++ ABC/ABC040/c.cpp
@@ -1,21 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N;
-const MAX_N = 100010;
-int a[MAX_N];
-long long INF = -1;
-long long dp[MAX_N][n];
-
-long long dfs(int n, int bef){
-
-}
+int dp[100010];
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
+    int n;
+    cin >> n;
+    vector<int> A(n);
+    for(int i = 0; i < n; i++){
+        cin >> A[i];
+    }
 
+    dp[0] = 0;
+    dp[1] = abs(A[1] - A[0]);
+
+    for(int i = 2; i < n; i++){
+        dp[i] = min(dp[i - 2] + abs(A[i] - A[i - 2]), dp[i - 1] + abs(A[i] - A[i - 1]));
+    }
+
+    cout << dp[n - 1] << endl;
 
 
     return 0;
