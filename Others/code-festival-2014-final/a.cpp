d69c708de2c385460b3c7b50b0e326b37b09fdc1
ozikot <dendai0122@gmail.com>
2019-02-08 20:31:57 +0900

    AC

---
+++ Others/code-festival-2014-final/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    double s;
+    cin >> s;
+
+    cout << 50 / s << endl;
+
+    return 0;
+}
