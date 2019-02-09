b872f524d54a82d60f903ceb5576f0ad3b0f7bab
ozikot <dendai0122@gmail.com>
2019-02-09 13:39:40 +0900

    AC

--- Others/joi2009yo/d.cpp
+++ Others/joi2009yo/d.cpp
@@ -1,73 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MAX = 95;
+int n, m, ans = 0;
+int A[MAX][MAX];
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+
+void dfs(int x, int y, int d){
+    ans = max(ans, d);
+    for(int i = 0; i < 4; i++){
+        int nx, ny;
+        nx = x + dx[i];
+        ny = y + dy[i];
+        if(nx < 0 or ny < 0 or nx >= n or ny >= m){
+            continue;
+        }
+        if(A[nx][ny] == 0) continue;
+        A[nx][ny] = 0;
+        dfs(nx, ny, d + 1);
+        A[nx][ny] = 1;
+    }
+    return;
+}
+
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
 
-    int m, n;
     cin >> m >> n;
-    int A[n][m];
-
-    vector<pair<int, int>> P;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
             cin >> A[i][j];
-            if(A[i][j] == 1) P.push_back({i, j});
         }
     }
 
-    int maxi = -1e9;
-    int D[n][m];
-    int dx[4] = {-1, 1, 0, 0};
-    int dy[4] = {0, 0, -1, 1};
-    for(int i = 0; i < P.size(); i++){
-        queue<pair<int, int>> q;
-        q.push({P[i].first, P[i].second});
+    for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            for(int k = 0; k < m; k++){
-                D[j][k] = 1e9;
-            }
+            if(A[i][j] == 0) continue;
+            A[i][j] = 0;
+            dfs(i, j, 1);
+            A[i][j] = 1;
         }
-        D[P[i].first][P[i].second] = 0;
-        for(int j = 0; j < n; j++){
-            for(int k = 0; k < m; k++){
-                cout << D[j][k] << ' ';
-            }
-            cout << endl;
-        }
-        cout << "-----------" << endl;
-        while(!q.empty()){
-            pair<int, int> p = q.front();
-            q.pop();
-            int x, y;
-            x = p.first;
-            y = p.second;
-            for(int j = 0; j < 4; j++){
-                int nx, ny;
-                nx = x + dx[j];
-                ny = y + dy[j];
-                if(nx < 0 or ny < 0 or nx >= n or ny >= m){
-                    continue;
-                }
-                if(A[nx][ny] == 0) continue;
-                if(D[nx][ny] > D[x][y] + 1){
-                    D[ny][ny] = D[x][y] + 1;
-                    maxi = max(maxi, D[x][y] + 1);
-                    q.push({nx, ny});
-                }
-            }
-        }
-        for(int j = 0; j < n; j++){
-            for(int k = 0; k < m; k++){
-                cout << D[j][k] << ' ';
-            }
-            cout << endl;
-        }
-        cout << "*************" << endl;
     }
 
-    cout << maxi << endl;
+    cout << ans << endl;
 
     return 0;
 }
