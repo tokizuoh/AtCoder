394f619d324784f4dbd92257feb422e8275ba5ec
ozikot <dendai0122@gmail.com>
2019-02-16 15:48:44 +0900

    AC

--- ABC/ABC114/d.cpp
+++ ABC/ABC114/d.cpp
@@ -7,43 +7,40 @@ int main(){
 
     int n;
     cin >> n;
-    vector<bool> F(n + 1, true);
-    F[0] = F[1] = false;
-
-    for(int i = 2; i <= sqrt(n); i++){
-        if(F[i]){
-            for(int j = i + i; j <= n; j += i){
-                F[j] = false;
-            }
-        }
-    }
-
-    vector<int> A;
-    for(int i = 0; i < n + 1; i++){
-        if(F[i]){
-            A.push_back(i);
-        }
-    }
-
-    map<int, int> mp;
+    
+    int p[25] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+    vector<int> d(25);
     for(int i = 2; i <= n; i++){
-        int k = i;
-        for(int j = 0; j < A.size(); j++){
-            while(true){
-                if(k % A[j] == 0){
-                    mp[A[j]]++;
-                    k /= A[j];
-                }else{
-                    break;
-                }
+        int a = i, j = 0;
+        while(a > 1){
+            if(a % p[j]){
+                j++;
+                if(j > 25) break;
+            }else{
+                a /= p[j];
+                d[j]++;
             }
         }
     }
 
-    for(auto x : mp){
-        cout << x.first << ' ' << x.second << endl;
+    int ans = 0;
+    for(int i = 0; i < 25; i++){
+        if(d[i] >= 74) ans++;
+        for(int j = i + 1; j < 25; j++){
+            if(i == j) continue;
+            if(d[i] >= 4 and d[j] >= 14) ans++;
+            if(d[i] >= 14 and d[j] >= 4) ans++;
+            if(d[i] >= 2 and d[j] >= 24) ans++;
+            if(d[i] >= 24 and d[j] >= 2) ans++;            
+            for(int k = j + 1; k < 25; k++){
+                if(i != k and d[i] >= 2 and d[j] >= 4 and d[k] >= 4) ans++;
+                if(i != k and d[i] >= 4 and d[j] >= 2 and d[k] >= 4) ans++;
+                if(i != k and d[i] >= 4 and d[j] >= 4 and d[k] >= 2) ans++;
+            }
+        }
     }
 
+    cout << ans << endl;
 
     return 0;
 }
