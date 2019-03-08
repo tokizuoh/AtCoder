5a73e98aa42bbdc2414cd46e5f11047e7781b51f
ozikot <dendai0122@gmail.com>
2019-03-08 22:51:25 +0900

    AC

---
+++ ABC/ABC039/c.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    string s;
+    cin >> s;
+
+    if(s == "WBWBWWBWBWBWWBWBWWBW") cout << "Do" << endl;
+    if(s == "WBWWBWBWBWWBWBWWBWBW") cout << "Re" << endl;
+    if(s == "WWBWBWBWWBWBWWBWBWBW") cout << "Mi" << endl;
+    if(s == "WBWBWBWWBWBWWBWBWBWW") cout << "Fa" << endl;
+    if(s == "WBWBWWBWBWWBWBWBWWBW") cout << "So" << endl;
+    if(s == "WBWWBWBWWBWBWBWWBWBW") cout << "La" << endl;
+    if(s == "WWBWBWWBWBWBWWBWBWWB") cout << "Si" << endl;
+
+    return 0;
+}
