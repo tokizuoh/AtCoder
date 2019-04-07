54d63270d6bbdcdf5e98cf6380c566d952def9ad
ozikot <dendai0122@gmail.com>
2019-04-07 13:02:46 +0900

    AC

---
+++ ABC/ABC123/c.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    long long n;
+    vector<long long> A(5);
+    cin >> n;
+    for(int i = 0; i < 5; i++){
+        cin >> A[i];
+    }
+
+    long long ans = 0;
+    for(int i = 0; i < n; i++){
+    
+    }
+
+
+
+    return 0;
+}
