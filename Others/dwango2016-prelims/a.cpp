d414cac2ea7410b4f8b532dc9498960de2e990f1
ozikot <dendai0122@gmail.com>
2019-04-18 19:26:09 +0900

    AC

---
+++ Others/dwango2016-prelims/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    cout << n / 25 << endl;      
+
+    return 0;
+}
