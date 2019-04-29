a56800e8676bd6a7f508333da60d7ef5d83afc9a
ozikot <dendai0122@gmail.com>
2019-04-29 14:09:34 +0900

    AC 要復習

--- Others/joisc2007/factorial.cpp
+++ Others/joisc2007/factorial.cpp
@@ -1,32 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define REP(i, n) for(int i = 0; i < n; i++)
 
 int main(){
-    cin.tie(0);
+    cin.tie(nullptr);
     ios::sync_with_stdio(false);
 
-    int n;
+    int n, t;
     cin >> n;
+    t = n;
 
-    int lim = sqrt(n);
-    vector<int> A(n + 1, 1);
-    A[0] = A[1] = 0;
-
-    for(int i = 2; i <= lim; i++){
-        if(A[i]){
-            for(int j = i * 2; j <= n; j += i){
-                A[j] = 0;       
-            }
+    // 素因数分解
+    map<int, int> mp;
+    for(int i = 2; i * i <= n; i++){
+        if(t % i != 0) continue;
+        while(t % i == 0){
+            mp[i]++;
+            t /= i;
         }
     }
+    if(t != 1) mp[t]++;
 
-    if(A[n] == 1){
-        cout << n << endl;
-        return 0;
+    // ok, ngの境界が明確なので二分探索可能
+    int ng = 1, ok = n;
+    while(ok - ng > 1){
+        int mid = (ng + ok) / 2;
+        bool f = true;
+        // nの素因数を使って調べる
+        for(auto i : mp){
+            int cnt = 0;
+            int x = i.first, y = i.second; // nの{prime, cnt}
+            while(mid >= x){
+                cnt += mid / x;
+                x *= i.first;
+            }
+            if(cnt < y){
+                f = false;
+                break;
+            }
+        }
+        if(f) ok = mid;
+        else ng = mid;
     }
 
-    
+    cout << ok << endl;
 
     return 0;
 }
