bdb718e70aaa3367c3b9abfda866e81080a6177a
ozikot <dendai0122@gmail.com>
2019-05-12 10:58:40 +0900

    AC

---
+++ Others/diverta2019/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    int n, k;
+    cin >> n >> k;
+    cout << n - k + 1 << endl;
+
+    return 0;
+}
